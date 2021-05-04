b332b88f2f7094c6e6c2147001876a38cb95b6c5
Bharat Dhaker <bharatkumardhaker@gmail.com>
2021-05-04 08:28:06 +0530

    Initial files added

---
+++ README.md
@@ -0,0 +1,23 @@
+# Laravel Trait Generator
+Laravel Generator For Creating New Traits
+
+## Installation
+
+Install using composer
+```
+composer require bkd27/trait-generator --dev
+```
+
+Service auto-discovered if laravel ^5.5 else Add the service provider, open `config/app.php`, and add a new item to the providers array.
+
+```
+Bkd27\TraitGenerator\ServiceProvider::class,
+```
+
+That's it! You're all set to go. Run the `php artisan` command from the Terminal to see the new `make:trait` command.
+
+## Usage
+
+```
+php artisan make:trait MyTrait
+```
---
+++ src/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bkd27\TraitGenerator;
+
+use Bkd27\TraitGenerator\Commands\TraitMakeCommand;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                TraitMakeCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
